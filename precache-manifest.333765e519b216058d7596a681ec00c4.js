5df1b0abd38fb5a4b812158a6419f3974e821bfc
root <infodelphustech@gmail.com>
2020-10-02 22:39:44 -0500

    deploy

---
+++ precache-manifest.333765e519b216058d7596a681ec00c4.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b664c649b01b9660a0de",
+    "url": "/my-project/css/about.692f7f96.css"
+  },
+  {
+    "revision": "2f4f663fa1231fbbe211",
+    "url": "/my-project/css/app.0f336871.css"
+  },
+  {
+    "revision": "0e1c25c1d704df8bfdf7",
+    "url": "/my-project/css/chunk-vendors.d184c4a1.css"
+  },
+  {
+    "revision": "6e745f52aa48cce6fadb171314ad6fd4",
+    "url": "/my-project/img/Loader.png"
+  },
+  {
+    "revision": "886073ff721ac1c88082f026476e6261",
+    "url": "/my-project/img/background.886073ff.png"
+  },
+  {
+    "revision": "827fdbede5ff23cfe71fc0cd851723d1",
+    "url": "/my-project/img/pikachu.827fdbed.png"
+  },
+  {
+    "revision": "827fdbede5ff23cfe71fc0cd851723d1",
+    "url": "/my-project/img/pikachu.png"
+  },
+  {
+    "revision": "2f9104d7babbd2eb84f15ada210a9b61",
+    "url": "/my-project/img/search.2f9104d7.svg"
+  },
+  {
+    "revision": "dccf0fc1e58cc2e8f2e1ada09ec72fa5",
+    "url": "/my-project/index.html"
+  },
+  {
+    "revision": "b664c649b01b9660a0de",
+    "url": "/my-project/js/about.9004ca7c.js"
+  },
+  {
+    "revision": "2f4f663fa1231fbbe211",
+    "url": "/my-project/js/app.24abdad8.js"
+  },
+  {
+    "revision": "0e1c25c1d704df8bfdf7",
+    "url": "/my-project/js/chunk-vendors.8aa2a3af.js"
+  },
+  {
+    "revision": "c6db0a64228772c78eafe99538e1ab39",
+    "url": "/my-project/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/my-project/robots.txt"
+  }
+]);
