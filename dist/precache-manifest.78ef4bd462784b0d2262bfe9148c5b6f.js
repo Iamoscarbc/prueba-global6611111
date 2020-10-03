db5bd2c126e06e4d72b38838b0eecb981455f096
Oscar Manuel Bravo Carbajal <oscarmbravoc@gmail.com>
2020-10-02 23:00:26 -0500

    cambios para el dist

---
+++ dist/precache-manifest.78ef4bd462784b0d2262bfe9148c5b6f.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b664c649b01b9660a0de",
+    "url": "/css/about.692f7f96.css"
+  },
+  {
+    "revision": "1f68bc8e59a6942e5185",
+    "url": "/css/app.0f336871.css"
+  },
+  {
+    "revision": "b078832cbf103b854661",
+    "url": "/css/chunk-vendors.d184c4a1.css"
+  },
+  {
+    "revision": "6e745f52aa48cce6fadb171314ad6fd4",
+    "url": "/img/Loader.png"
+  },
+  {
+    "revision": "886073ff721ac1c88082f026476e6261",
+    "url": "/img/background.886073ff.png"
+  },
+  {
+    "revision": "827fdbede5ff23cfe71fc0cd851723d1",
+    "url": "/img/pikachu.827fdbed.png"
+  },
+  {
+    "revision": "827fdbede5ff23cfe71fc0cd851723d1",
+    "url": "/img/pikachu.png"
+  },
+  {
+    "revision": "2f9104d7babbd2eb84f15ada210a9b61",
+    "url": "/img/search.2f9104d7.svg"
+  },
+  {
+    "revision": "bee306cec15fb30f56251e3ea45a083f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b664c649b01b9660a0de",
+    "url": "/js/about.9004ca7c.js"
+  },
+  {
+    "revision": "1f68bc8e59a6942e5185",
+    "url": "/js/app.540d9e1e.js"
+  },
+  {
+    "revision": "b078832cbf103b854661",
+    "url": "/js/chunk-vendors.d8f76237.js"
+  },
+  {
+    "revision": "c6db0a64228772c78eafe99538e1ab39",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
